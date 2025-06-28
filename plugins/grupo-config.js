b32cea718a327cc4ee5fbb5c97566941d7240fcf
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-06-28 03:03:17 -0300

    Update grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -1,9 +1,30 @@
-const handler = async (m, { conn, command }) => {
-  await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icono)
+let isClose = { // Switch Case Like :v
+'open': 'not_announcement',
+'close': 'announcement',
+'abierto': 'not_announcement',
+'cerrado': 'announcement',
+'abrir': 'not_announcement',
+'cerrar': 'announcement',
+}[(args[0] || '')]
+if (isClose === undefined)
+return conn.reply(m.chat, ${emoji} *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} close*\n*✰ #${command} open*, m)
+await conn.groupSettingUpdate(m.chat, isClose)
+
+if (isClose === 'not_announcement'){
+m.reply(${emoji} )
 }
 
-handler.command = /^open|close$/i
-handler.owner = false
-handler.admin = false
+if (isClose === 'announcement'){
+m.reply(${emoji2} )
+}}
+handler.help = ['group open / close', 'grupo abrir / cerrar']
+handler.tags = ['grupo']
+handler.command = ['group', 'grupo']
+handler.admin = true
+handler.botAdmin = true
 
 export default handler
+
+
